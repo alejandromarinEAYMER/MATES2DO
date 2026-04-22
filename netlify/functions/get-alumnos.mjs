22dffcaa36f307360c1d753ede8abffb3b5fd017
alejandromarinEAYMER <149966930+alejandromarinEAYMER@users.noreply.github.com>
2026-04-22 18:14:38 +0200

    Update get-alumnos.mjs

--- netlify/functions/get-alumnos.mjs
+++ netlify/functions/get-alumnos.mjs
@@ -21,21 +21,21 @@ export default async (req, context) => {
     if (!data) {
       // Alumnos iniciales con 0 puntos
       const alumnosIniciales = [
-        { id: 1, nombre: "Alumno 1", puntos: 0, avatar: "🦁" },
-        { id: 2, nombre: "Alumno 2", puntos: 0, avatar: "🐯" },
-        { id: 3, nombre: "Alumno 3", puntos: 0, avatar: "🦊" },
-        { id: 4, nombre: "Alumno 4", puntos: 0, avatar: "🐺" },
-        { id: 5, nombre: "Alumno 5", puntos: 0, avatar: "🦅" },
-        { id: 6, nombre: "Alumno 6", puntos: 0, avatar: "🐉" },
-        { id: 7, nombre: "Alumno 7", puntos: 0, avatar: "🦋" },
-        { id: 8, nombre: "Alumno 8", puntos: 0, avatar: "🐬" },
-        { id: 9, nombre: "Alumno 9", puntos: 0, avatar: "🦄" },
-        { id: 10, nombre: "Alumno 10", puntos: 0, avatar: "🐲" },
-        { id: 11, nombre: "Alumno 11", puntos: 0, avatar: "🦈" },
-        { id: 12, nombre: "Alumno 12", puntos: 0, avatar: "🐻" },
-        { id: 13, nombre: "Alumno 13", puntos: 0, avatar: "🦝" },
-        { id: 14, nombre: "Alumno 14", puntos: 0, avatar: "🐙" },
-        { id: 15, nombre: "Alumno 15", puntos: 0, avatar: "🦖" },
+        { id: 1, nombre: "Ariana Baez", puntos: 0, avatar: "🦁" },
+        { id: 2, nombre: "Marcos Dávila", puntos: 0, avatar: "🐯" },
+        { id: 3, nombre: "Alejandro Gago", puntos: 0, avatar: "🦊" },
+        { id: 4, nombre: "Nora González", puntos: 0, avatar: "🐺" },
+        { id: 5, nombre: "Illán Iglesias", puntos: 0, avatar: "🦅" },
+        { id: 6, nombre: "Prestone Kabu", puntos: 0, avatar: "🐉" },
+        { id: 7, nombre: "Álex Lozano", puntos: 0, avatar: "🦋" },
+        { id: 8, nombre: "Daniela Marcos", puntos: 0, avatar: "🐬" },
+        { id: 9, nombre: "Irene Marín", puntos: 0, avatar: "🦄" },
+        { id: 10, nombre: "Amy Medina", puntos: 0, avatar: "🐲" },
+        { id: 11, nombre: "Alejandro Osal", puntos: 0, avatar: "🦈" },
+        { id: 12, nombre: "Alejandro Parrondo", puntos: 0, avatar: "🐻" },
+        { id: 13, nombre: "Adrián Salinas", puntos: 0, avatar: "🦝" },
+        { id: 14, nombre: "Adriana Severiche", puntos: 0, avatar: "🐙" },
+        { id: 15, nombre: "Marina Villalba", puntos: 0, avatar: "🦖" },
       ];
       await store.setJSON("ranking", alumnosIniciales);
       return new Response(JSON.stringify(alumnosIniciales), {
